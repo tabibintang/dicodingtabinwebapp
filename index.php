3c0604fd969661838a2694f76bd3f7140b23e80f
BINTANG-ROG\tabibintang <tabibintang@gmail.com>
2019-05-02 01:22:13 +0700

    tes t function

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.css">
 	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
 </head>
 <body>
 	<div class="col-md-12">
@@ -151,10 +151,9 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 			          <td>
 			          	<textarea id="responseTextArea<?php echo $user['id_user'] ?>" class="UIInput" style="width:580px; height:400px;"></textarea>
 
-			          	<?php echo"<script type='text/javascript'>
-			          		ngeAlert(".$user['id_user'].",".$avatarurl.");
-			          	</script>";
-			          	?>
+			          	<script type='text/javascript'>
+			          		ngeAlert("1","URL");
+			          	</script>
 			          </td>
 			        </tr>
 		        <?php 
@@ -163,7 +162,7 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 	      </tbody>
 	    </table>
 	</div>
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
