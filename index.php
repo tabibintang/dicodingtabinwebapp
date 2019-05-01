8a101c23b109b6574ca9ac18cc98cc1e346ae859
BINTANG-ROG\tabibintang <tabibintang@gmail.com>
2019-05-02 01:08:01 +0700

    fix analyze image

--- index.php
+++ index.php
@@ -212,9 +212,10 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 			          <td><?php echo $user['blockblob'] ?></td>
 			          <td><img style="width: 200px " src="<?php echo $avatarurl ?>"></td>
 			          <td>
-			          	<script type="text/javascript">
+			          	<?php echo"<script type='text/javascript'>
 			          		processImage(<?php echo $user['id_user'] ?>,<?php echo $avatarurl ?>);
-			          	</script>
+			          	</script>";
+			          	?>
 			          	<textarea id="responseTextArea<?php echo $user['id_user'] ?>" class="UIInput" style="width:580px; height:400px;"></textarea></td>
 			        </tr>
 		        <?php 
